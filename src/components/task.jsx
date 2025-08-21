0a0694237f8c0b4c3198c4abc1638e8da8d70234
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-22 08:15:12 +0900

    checked

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -4,7 +4,7 @@ export const Task = (props) => {
       <div className="flex gap-2.5 items-center">
         <input
           type="checkbox"
-          // checked
+          checked={props.isCompleted}
           className="w-5 h-5 rounded-xs bg-red-700 border border-[#767676]"
         ></input>
         <p
