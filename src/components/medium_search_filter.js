676218c71168dde5ce1ec77b937cc0ee89105dfa
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-29 09:45:28 +0800

    added

--- src/components/medium_search_filter.js
+++ src/components/medium_search_filter.js
@@ -7,8 +7,7 @@ const items = ["Apple", "Banana", "Carrot", "Date", "Eggplant", "Fig", "Grape"];
 
 export function SearchFilter() {
   const [inputValue, setInputValue] = useState("");
-  const [filteresItems, setFilteredItems] = useState(items);
-  console.log(items);
+
   const handleOnChange = (event) => {
     setInputValue(event.target.value);
   };
@@ -21,14 +20,17 @@ export function SearchFilter() {
         value={inputValue}
       ></input>
       <ul>
-        {items.map((item, id) => (
-          <li key={id}>{item}</li>
-        ))}
-        {filteresItems.filter(
-          (item, id) =>
-            item.toLowerCase().includes(inputValue) && <li>{item}</li>
-        )}
+        {items
+          .filter((item) => item.toLowerCase().includes(inputValue))
+          .map((el) => (
+            <li key={el}>{el}</li>
+          ))}
       </ul>
     </div>
   );
+}
+{
+  /* {items.filter(
+          (item) => item.toLowerCase().includes(inputValue) && <li>{item}</li>
+        )} */
 }
